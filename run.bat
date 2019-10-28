74b3049a0f34fc74d1592c925d2de7414f1cc4d2
Ixarias <lacareo@gmail.com>
2019-10-27 22:10:55 -0500

    Near-Completion Commit #2

--- run.bat
+++ run.bat
@@ -1,20 +1,7 @@
 @ECHO OFF
 mode con: cols=120 lines=40
-:: ORB3
-ECHO Orb3
-start /W /B /ABOVENORMAL "run" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /ABOVENORMAL "run2" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /ABOVENORMAL "run3" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /ABOVENORMAL "run4" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /ABOVENORMAL "run5" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /ABOVENORMAL "run6" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
 :: ORB4
-xcopy /Y C:\Users\Victor\Documents\GitHub\EvoComp2\orb4ms.txt C:\Users\Victor\Documents\GitHub\EvoComp2\MachineSequences.txt
-xcopy /Y C:\Users\Victor\Documents\GitHub\EvoComp2\orb4pt.txt C:\Users\Victor\Documents\GitHub\EvoComp2\ProcessingTime.txt
 ECHO Orb4
-start /W /B /HIGH "run" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /HIGH "run2" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /HIGH "run3" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
 start /W /B /HIGH "run4" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
 start /W /B /HIGH "run5" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
 start /W /B /HIGH "run6" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
@@ -64,6 +51,20 @@ start /W /B /HIGH "run7" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
 start /W /B /HIGH "run8" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
 start /W /B /HIGH "run9" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
 start /W /B /HIGH "run10" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
+:: ft6
+xcopy /Y C:\Users\Victor\Documents\GitHub\EvoComp2\ft6ms.txt C:\Users\Victor\Documents\GitHub\EvoComp2\MachineSequences.txt
+xcopy /Y C:\Users\Victor\Documents\GitHub\EvoComp2\ft6pt.txt C:\Users\Victor\Documents\GitHub\EvoComp2\ProcessingTime.txt
+ECHO ft6
+start /W /B /HIGH "run" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
+start /W /B /HIGH "run2" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
+start /W /B /HIGH "run3" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
+start /W /B /HIGH "run4" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
+start /W /B /HIGH "run5" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
+start /W /B /HIGH "run6" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
+start /W /B /HIGH "run7" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
+start /W /B /HIGH "run8" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
+start /W /B /HIGH "run9" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
+start /W /B /HIGH "run10" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
 :: ft10
 xcopy /Y C:\Users\Victor\Documents\GitHub\EvoComp2\ft10ms.txt C:\Users\Victor\Documents\GitHub\EvoComp2\MachineSequences.txt
 xcopy /Y C:\Users\Victor\Documents\GitHub\EvoComp2\ft10pt.txt C:\Users\Victor\Documents\GitHub\EvoComp2\ProcessingTime.txt
@@ -82,20 +83,6 @@ start /W /B /HIGH "run10" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
 xcopy /Y C:\Users\Victor\Documents\GitHub\EvoComp2\ft20ms.txt C:\Users\Victor\Documents\GitHub\EvoComp2\MachineSequences.txt
 xcopy /Y C:\Users\Victor\Documents\GitHub\EvoComp2\ft20pt.txt C:\Users\Victor\Documents\GitHub\EvoComp2\ProcessingTime.txt
 ECHO ft20
-start /W /B /HIGH "run" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /HIGH "run2" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /HIGH "run3" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /HIGH "run4" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /HIGH "run5" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /HIGH "run6" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /HIGH "run7" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /HIGH "run8" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /HIGH "run9" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-start /W /B /HIGH "run10" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
-:: la5
-xcopy /Y C:\Users\Victor\Documents\GitHub\EvoComp2\la5ms.txt C:\Users\Victor\Documents\GitHub\EvoComp2\MachineSequences.txt
-xcopy /Y C:\Users\Victor\Documents\GitHub\EvoComp2\la5pt.txt C:\Users\Victor\Documents\GitHub\EvoComp2\ProcessingTime.txt
-ECHO ft10
 start /W /B /HIGH "run" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
 start /W /B /HIGH "run2" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
 start /W /B /HIGH "run3" C:\Users\Victor\Documents\GitHub\EvoComp2\a.exe
